b6bdfc10d554704b65c73149278e57d643aed86f
razin-ivan98 <razin-ivan98@ya.ru>
2020-08-21 10:40:10 +0300

    webpack build

---
+++ src/components/core/Button/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { ButtonWrapper } from "./Button.styled";
+
+const Button = () => (
+	<ButtonWrapper>HelloWorld</ButtonWrapper>
+);
+
+export default Button;
---
+++ src/components/core/Button/Button.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.button`
+	width: auto;
+	height: 22px;
+	background-color: green;
+	color: white;
+`;
